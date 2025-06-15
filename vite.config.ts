40b202f659f203afa0abd9dd6ec5e3e22b68c9a9
Yusoof Moh <me@yusoofsh.id>
2025-06-15 08:26:56 +0700

    disabkle pwa temp

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import tailwindcss from "@tailwindcss/vite"
 import tanstackRouter from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { VitePWA as pwa } from "vite-plugin-pwa"
 
 export default defineConfig({
 	build: { outDir: "dist" },
@@ -11,22 +10,22 @@ export default defineConfig({
 		react(),
 		tailwindcss(),
 		tanstackRouter(),
-		pwa({
-			registerType: "autoUpdate",
-			manifest: {
-				name: "Moscloc",
-				short_name: "moscloc",
-				description: "Moscloc - PWA Application",
-				theme_color: "#0c0c0c",
-			},
-			pwaAssets: {
-				disabled: false,
-				config: true,
-			},
-			devOptions: {
-				enabled: true,
-			},
-		}),
+		// pwa({
+		// 	registerType: "autoUpdate",
+		// 	manifest: {
+		// 		name: "Moscloc",
+		// 		short_name: "moscloc",
+		// 		description: "Moscloc - PWA Application",
+		// 		theme_color: "#0c0c0c",
+		// 	},
+		// 	pwaAssets: {
+		// 		disabled: false,
+		// 		config: true,
+		// 	},
+		// 	devOptions: {
+		// 		enabled: true,
+		// 	},
+		// }),
 	],
 	resolve: {
 		alias: {
